4a1fa9f9f8d949191deb52156befaecaa5031773
Lindo <ldmasilela@gmail.com>
2019-06-04 11:30:31 -0700

    added default styles

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,34 @@
 import Header from './header';
 import Footer from './footer';
+import Head from 'next/head';
 
 const Layout = props => {
   return (
     <div className="container">
+      <Head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>>
+
+        <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,700|Anton" rel="stylesheet"/>
+        
+
+        <link rel="stylesheet" href="static/fonts/icomoon/style.css"/>
+
+        <link rel="stylesheet" href="static/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="static/css/magnific-popup.css"/>
+        <link rel="stylesheet" href="static/css/jquery-ui.css"/>
+        <link rel="stylesheet" href="static/css/owl.carousel.min.css"/>
+        <link rel="stylesheet" href="static/css/owl.theme.default.min.css"/>
+
+        <link rel="stylesheet" href="static/css/bootstrap-datepicker.css"/>
+
+        <link rel="stylesheet" href="static/fonts/flaticon/font/flaticon.css"/>
+
+        <link rel="stylesheet" href="static/css/aos.css"/>
+
+        <link rel="stylesheet" href="static/css/style.css"/>
+      </Head>
+
       <Header/>
       {props.children}
       <Footer/>
--- components/header/index.js
+++ components/header/index.js
@@ -3,11 +3,11 @@ import Link from "next/link";
 const Header = () => {
   return (
     <div className="nav-container">
-      <Link href="/index" as="home"> Home </Link>
-      <Link href="/OurStoryPage" as="our-story">Our Story</Link>
-      <Link href="/EventsPage" as="events"> Events </Link>
-      <Link href="/Donate" as="our-story">Donate</Link>
-      <Link href="/MediaPage" as="our-story">Media</Link>
+      <Link href="/" prefetch ><a>Home</a></Link>
+      <Link href="/OurStoryPage" as="our-story"><a>Our Story</a></Link>
+      <Link href="/EventsPage" as="events"><a>Events</a></Link>
+      <Link href="/Donate" as="our-story"><a>Donate</a></Link>
+      <Link href="/MediaPage" as="our-story"><a>Media</a></Link>
     </div>
   )
 }
