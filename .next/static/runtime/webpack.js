f364874dff15307a82b808812ef6593e27b57529
alusru <collen.nkabinde@gmail.com>
2019-06-07 21:26:56 +0200

    contact us page sorted

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1c69b57c72ab3ae976a0";
+/******/ 	var hotCurrentHash = "18795c350dba9ce7450f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/ContactPage.js
+++ pages/ContactPage.js
@@ -1,10 +1,12 @@
 import Layout from '../components/Layout';
 import Form from '../components/contact/index';
+import Mini from '../components/header/mini_slider';
 
 const ContactPage = () => {
 	return (
 		<Layout>
 			<div className="content">
+				<Mini />
 				<Form />
 			</div>
 		</Layout>
